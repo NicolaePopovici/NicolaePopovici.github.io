2a37373a40ea311a851c2a3a9921e5922cf66f33
NicolaePopovici <for.nicolae.popovici@gmail.com>
2022-03-08 17:30:34 +0100

    dummy generator

---
+++ sitemap_generator.rb
@@ -0,0 +1,19 @@
+require 'builder'
+
+NUMBER_OF_ENTRIES = 3000
+
+def product_xml
+  xml = Builder::XmlMarkup.new( :indent => 2 )
+  xml.instruct! :xml, :encoding => "UTF-8"
+
+  xml.urlset(xmlns: "http://www.sitemaps.org/schemas/sitemap/0.9") do |p|
+    NUMBER_OF_ENTRIES.times {
+      xml.url do |u|
+        u.loc "https://nicolaepopovici.github.io/lorem_ipsum.html"
+        u.lastmod "2021-11-16T19:40:02+01:00"
+      end
+    }
+  end
+end
+
+File.write("sitemap.xml", product_xml)
